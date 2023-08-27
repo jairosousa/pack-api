ad4c0662a4ab99820f653e473ee6074df54d5875
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-27 16:41:29 -0300

    Classe de Entidade Usuario
     Changes to be committed:
    	new file:   src/main/java/com/jnsdev/parkapi/entity/Usuario.java

---
+++ src/main/java/com/jnsdev/parkapi/entity/Usuario.java
@@ -0,0 +1,73 @@
+package com.jnsdev.parkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 27/08/2023 - 16:12
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Column(name = "role", nullable = false, length = 25)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriação;
+
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENTE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
+}
