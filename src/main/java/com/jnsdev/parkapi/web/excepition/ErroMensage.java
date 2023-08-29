4706175e2662483cc9395f3053142f55e041f0d0
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-29 14:21:33 -0300

    Objeto de erro para respostas de requisições
     Changes to be committed:
    	new file:   src/main/java/com/jnsdev/parkapi/web/excepition/ErroMensage.java

---
+++ src/main/java/com/jnsdev/parkapi/web/excepition/ErroMensage.java
@@ -0,0 +1,53 @@
+package com.jnsdev.parkapi.web.excepition;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 29/08/2023 - 14:09
+ */
+@Getter
+@ToString
+public class ErroMensage {
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+    private Map<String, String> erros;
+
+    public ErroMensage() {
+    }
+
+    public ErroMensage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.path = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErroMensage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.path = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErros(result);
+    }
+
+    private void addErros(BindingResult result) {
+        this.erros = new HashMap<>();
+        for (FieldError fieldError : result.getFieldErrors()) {
+            this.erros.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
