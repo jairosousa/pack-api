7d3bceda85e8e50bc490e70f75bc7699e16a0bd4
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-30 14:48:28 -0300

    Teste ponta a ponta para criar um novo usuário  Changes to be committed: 	deleted:    src/test/java/UsuarioIT.java 	new file:   src/test/java/com/jnsdev/parkapi/UsuarioIT.java 	modified:   src/test/resources/application.yaml 	modified:   src/test/resources/sql/usuarios/usuarios-delete.sql 	new file:   src/test/resources/sql/usuarios/usuarios-insert.sql 	deleted:    src/test/resources/sql/usuarios/usuarios_insert.sql

---
+++ src/test/java/com/jnsdev/parkapi/UsuarioIT.java
@@ -0,0 +1,41 @@
+package com.jnsdev.parkapi;
+
+import com.jnsdev.parkapi.web.dto.UsuarioCreateDto;
+import com.jnsdev.parkapi.web.dto.UsuarioResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 30/08/2023 - 11:36
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void createUsuario_ComUsernameEPasswordValidos_RetornarUsuariosCriadoComStatus201() {
+        UsuarioResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("tody@email.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE");    }
+}
