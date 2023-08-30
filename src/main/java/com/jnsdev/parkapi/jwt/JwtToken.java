378601596cdb06d7d1e5e1f293502b2cbb60298a
Jairo Nascimento <jaironsousa@gmail.com>
2023-08-30 18:33:40 -0300

    Criando o objeto de resposta JwtToken
     Changes to be committed:
    	new file:   src/main/java/com/jnsdev/parkapi/jwt/JwtToken.java

---
+++ src/main/java/com/jnsdev/parkapi/jwt/JwtToken.java
@@ -0,0 +1,19 @@
+package com.jnsdev.parkapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 30/08/2023 - 18:32
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+    private String token;
+}
