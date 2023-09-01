9eb15840aac34b81a45e84dafa1e88d779f09658
Jairo Nascimento <jaironsousa@gmail.com>
2023-09-01 11:45:29 -0300

    Preparando a operação de autenticação para os testes
     Changes to be committed:
    	new file:   src/test/java/com/jnsdev/parkapi/JwtAuthentication.java

---
+++ src/test/java/com/jnsdev/parkapi/JwtAuthentication.java
@@ -0,0 +1,28 @@
+package com.jnsdev.parkapi;
+
+import com.jnsdev.parkapi.jwt.JwtToken;
+import com.jnsdev.parkapi.web.dto.UsuarioLoginDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.function.Consumer;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 01/09/2023 - 11:39
+ */
+public class JwtAuthentication {
+
+    public static Consumer<HttpHeaders> getHeaderAuthorization(WebTestClient client, String username, String password) {
+        String token = client
+                .post()
+                .uri("/api/vi/auth")
+                .bodyValue(new UsuarioLoginDto(username, password))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(JwtToken.class)
+                .returnResult().getResponseBody().getToken();
+
+        return headers -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+    }
+}
