5b4a344b8351cef4c8f713448a9a81b94c2aba90
Jairo Nascimento <jaironsousa@gmail.com>
2023-09-02 18:05:10 -0300

    Teste ponto a ponto para Create (Status 201)
     Changes to be committed:
    	modified:   src/main/java/com/jnsdev/parkapi/web/controller/ClienteController.java
    	new file:   src/test/java/com/jnsdev/parkapi/ClienteIT.java
    	new file:   src/test/resources/sql/clientes/clientes-delete.sql
    	new file:   src/test/resources/sql/clientes/clientes-insert.sql

--- src/main/java/com/jnsdev/parkapi/web/controller/ClienteController.java
+++ src/main/java/com/jnsdev/parkapi/web/controller/ClienteController.java
@@ -7,6 +7,13 @@ import com.jnsdev.parkapi.service.UsuarioService;
 import com.jnsdev.parkapi.web.dto.ClienteCreateDto;
 import com.jnsdev.parkapi.web.dto.ClienteResponseDto;
 import com.jnsdev.parkapi.web.dto.mapper.ClienteMapper;
+import com.jnsdev.parkapi.web.excepition.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Created 02/09/2023 - 17:22
  */
 
+@Tag(name = "Clientes", description = "Contém todas as opereções relativas ao recurso de um cliente")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/clientes")
@@ -30,6 +38,20 @@ public class ClienteController {
     private final ClienteService clienteService;
     private final UsuarioService usuarioService;
 
+    @Operation(summary = "Criar um novo cliente",
+            description = "Recurso para criar um novo cliente vinculado a um usuário cadastrado. " +
+                    "Requisição exige uso de um bearer token. Acesso restrito a Role='CLIENTE'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já possui cadastro no sistema",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de ADMIN",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     @PreAuthorize("hasRole('CLIENTE')")
     public ResponseEntity<ClienteResponseDto> create(@Valid @RequestBody ClienteCreateDto dto,
---
+++ src/test/java/com/jnsdev/parkapi/ClienteIT.java
@@ -0,0 +1,43 @@
+package com.jnsdev.parkapi;
+
+import com.jnsdev.parkapi.web.dto.ClienteCreateDto;
+import com.jnsdev.parkapi.web.dto.ClienteResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 02/09/2023 - 17:46
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ClienteIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void criarCliente_ComDadosValidos_RetornarClienteComStatus201() {
+        ClienteResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Tobias Ferreira", "91191064085"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ClienteResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNome()).isEqualTo("Tobias Ferreira");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("91191064085");
+    }
+}
