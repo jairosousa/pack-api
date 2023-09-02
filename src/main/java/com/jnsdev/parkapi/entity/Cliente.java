a1b949592d073c50b5df69fb447e52a8941bb010
Jairo Nascimento <jaironsousa@gmail.com>
2023-09-02 07:39:33 -0300

    Criando e mapeando a entidade Cliente
     Changes to be committed:
    	new file:   src/main/java/com/jnsdev/parkapi/entity/Cliente.java

---
+++ src/main/java/com/jnsdev/parkapi/entity/Cliente.java
@@ -0,0 +1,72 @@
+package com.jnsdev.parkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 27/08/2023 - 16:12
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
+@EntityListeners(AuditingEntityListener.class)
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriação;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
